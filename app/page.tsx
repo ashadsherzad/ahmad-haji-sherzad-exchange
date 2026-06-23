9d4f072aa8848c68ab5d402f9618fec6f418794d
ashadsherzad <EMAIL_GITHUB>
2026-06-23 08:19:02 -0700

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-7xl font-bold text-red-500">
-          TEST WEBSITE
-        </h1>
-
-        <p className="text-3xl mt-6">
-          GitHub Update Success ✅
-        </p>
-      </div>
+    <main>
+      <h1>AHMAD HAJI SHERZAD EXCHANGE</h1>
+      <p>Welcome to our exchange website.</p>
     </main>
   );
 }
