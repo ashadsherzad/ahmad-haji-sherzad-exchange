fde29b003b37d4d91fa034c6c4703c9f74eafdfd
ashadsherzad <EMAIL_GITHUB>
2026-06-23 08:14:15 -0700

    move next config

--- app/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/ahmad-haji-sherzad-exchange",
-  assetPrefix: "/ahmad-haji-sherzad-exchange/",
-};
-
-export default nextConfig;
