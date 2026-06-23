01739a47a3c5ac8eca489d671baccc2dcee32bd4
ashadsherzad <EMAIL_GITHUB>
2026-06-23 08:04:22 -0700

    add github pages config

---
+++ app/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/ahmad-haji-sherzad-exchange",
+  assetPrefix: "/ahmad-haji-sherzad-exchange/",
+};
+
+export default nextConfig;
