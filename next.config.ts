8408b224ed6020c2d53d0cd9d45ba891aa92a7d0
ashadsherzad <EMAIL_GITHUB>
2026-06-23 08:56:59 -0700

    fix github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/ahmad-haji-sherzad-exchange",
+  assetPrefix: "/ahmad-haji-sherzad-exchange/",
 };
 
 export default nextConfig;
